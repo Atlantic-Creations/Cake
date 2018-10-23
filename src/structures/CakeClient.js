c43887c34ac06da2c7bf98458d3619f4025b9689
SamuraiStackszz <38820196+SamuraiStacks@users.noreply.github.com>
2018-10-22 20:13:42 -0400

    fixed something

--- src/structures/CakeClient.js
+++ src/structures/CakeClient.js
@@ -10,40 +10,35 @@ class CakeClient extends Eris {
         this.botinfo = require("../../data/botinfo.json")
         this.commands = new Map()
         this.aliases = new Map()
-    }
 
-    loadCommands() {
-        fs.readdir("../commands/", (err, files) => {
-            let cmds = files.filter(f => f.split(".").pop() === "js")
-            if(cmds.length === 0) {
-              console.log("There are no commands for me to load!")
-              return;
-            }
-            cmds.forEach(f => {
-              let props = require(`../../commands/${f}`)
-              if(props.settings.disabled) {
-                loggr.info(`${f.slice(0, -3)} has been disabled, reason: ${props.settings.disabled.reason}`)
-                props.file = f
-                boat.commands.set(props.info.name, props)
-                props.settings.aliases.forEach(a => {
-                  boat.aliases.set(a, props.info.name)
-                })
+        this.loadCommands = function() {
+          fs.readdir("./commands/", (err, files) => {
+              let cmds = files.filter(f => f.split(".").pop() === "js")
+              if(cmds.length === 0) {
+                loggr.info("There are no commands for me to load!")
                 return;
-              } else {
-                console.time()
-                props.file = f
-                boat.commands.set(props.info.name, props)
-                props.settings.aliases.forEach(a => {
-                  boat.aliases.set(a, props.info.name)
-                })
-                loggr.info(`${f.slice(0, -3)} has loaded after ${console.timeEnd()}`)
               }
-            })
-        })
-    }
-
-    bake() {
-        this.connect()
+              cmds.forEach(f => {
+                let props = require(`../../commands/${f}`)
+                if(props.settings.disabled) {
+                  loggr.info(`${f.slice(0, -3)} has been disabled, reason: ${props.settings.disabled.reason}`)
+                  props.file = f
+                  this.commands.set(props.info.name, props)
+                  props.settings.aliases.forEach(a => {
+                    this.aliases.set(a, props.info.name)
+                  })
+                  return;
+                } else {
+                  props.file = f
+                  this.commands.set(props.info.name, props)
+                  props.settings.aliases.forEach(a => {
+                    this.aliases.set(a, props.info.name)
+                  })
+                  loggr.info(`${f.slice(0, -3)} has finished loading!`)
+                }
+              })
+          })
+      }
     }
 }
 
