6d798fbaade288896d0894db042bb848dd952708
SamuraiStackszz <38820196+SamuraiStacks@users.noreply.github.com>
2018-10-23 07:08:55 -0400

    no errors if a command is unknown

--- cake.js
+++ cake.js
@@ -46,7 +46,24 @@ cake.on("messageCreate", msg => {
           cake.botinfo.cmds_ran++
         }
       } catch (err) {
+        if(err.message === "Cannot read property 'get' of undefined") return;
         console.log(err)
       }  
 })
 cake.connect()
+
+Map.map = function(fn, thisArg) {
+  if (thisArg) fn = fn.bind(thisArg);
+  const arr = new Array(this.size);
+  let i = 0;
+  for (const [key, val] of this) arr[i++] = fn(val, key, this);
+  return arr;
+}
+Map.filter = function(fn, thisArg) {
+    if (thisArg) fn = fn.bind(thisArg);
+    const results = new Collection();
+    for (const [key, val] of this) {
+      if (fn(val, key, this)) results.set(key, val);
+    }
+    return results;
+}
